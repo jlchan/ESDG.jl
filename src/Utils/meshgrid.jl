99e2abaa918fce84d027850f8fc64ad5226ff8a9
Jesse Chan <jessechan@Jesses-MacBook-Pro.local>
2019-10-21 14:07:46 -0500

    adding 3D meshgrid (non-matlab ordering)

--- src/Utils/meshgrid.jl
+++ src/Utils/meshgrid.jl
@@ -27,3 +27,31 @@ Matlab meshgrid function. Return 2D grid coordinates according to x and y=x.
 function meshgrid(x)
     return meshgrid(x,x)
 end
+
+"""
+    meshgrid(x,y,z)
+
+A hacky version of meshgrid which returns vectors, not tensors
+
+# Examples
+```jldoctest
+```
+"""
+function meshgrid(x1D,y1D,z1D)
+        Np = length(x1D)*length(y1D)*length(z1D)
+        x = zeros(Np)
+        y = zeros(Np)
+        z = zeros(Np)
+        sk = 1
+        for k = 1:length(z1D)
+                for j = 1:length(y1D)
+                        for i = 1:length(x1D)
+                                x[sk] = x1D[i]
+                                y[sk] = y1D[j]
+                                z[sk] = z1D[k]
+                                sk += 1
+                        end
+                end
+        end
+        return x,y,z
+end
