ed6eaf3b18238bb4e040af8645349f5839e44390
Jesse Chan <jchan985@gmail.com>
2019-11-22 18:20:10 -0600

    test

--- test.jl
+++ test.jl
@@ -1,29 +1,51 @@
-# function counter()
-#     n = 0
-#     ()->n+=1
+using BenchmarkTools
+using StaticArrays
+
+N = 2
+u,v = ntuple(x->randn(N),2)
+
+function foo!(u,v)
+    u .= v
+end
+
+@show u
+foo!(u,v)
+@show u
+
+# foo1(U) = (x->x[:,1]).(U)
+# foo2(U) = tuple((x->x[:,1]).(U)...)
+# @btime foo1($U)
+# @btime foo2($U)
+
+# N = 10000
+# a,b,c,d = ntuple(x->randn(N),4)
+# X = (a,b)
+# Y = (c,d)
+# X = [a,b]
+# Y = [c,d]
+
+# X = SizedVector{2}(a,b)
+# Y = SizedVector{2}(c,d)
+
+#
+# function foo(a,b,c,d)
+#     ab = exp(a*b)
+#     return ab+c, ab-d
 # end
 #
-# addOne = counter()
-# addOneAgain = counter()
+# function foo_loop(N,a,b,c,d)
+#     for i = 1:N
+#         foo(a[i],b[i],c[i],d[i])
+#     end
+# end
+# @btime foo_loop($N,$a,$b,$c,$d)
 #
-# @show addOne()
-# @show addOne()
-# @show addOneAgain()
-
-using BenchmarkTools
-
-N = 3
-X = (zeros(N),zeros(N))
-Y = [1,2]
-function foo1!(X,Y,i)
-    for fld in eachindex(X)
-        X[fld][i] = Y[fld]
-    end
-end
-@btime foo1!($X,$Y,$1)
-
-foo2!(x,y,i) = x[i] = y
-@btime foo2!.($X,$Y,$1)
-
-foo3!(X,Y,i) = getindex(X,i) = Y
-@btime foo3!($X,$Y,$1)
+# function foo_loop_splat!(N,X,Y)
+#     for i = 1:N
+#         foo(getindex.(X,i)...,getindex.(Y,i)...)
+#     end
+# end
+# @btime foo_loop_splat!($N,$X,$Y)
+#
+# # replace tuples with sized vectors
+# @btime foo_loop_splat!($N,$X,$Y)
