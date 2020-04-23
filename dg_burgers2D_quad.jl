eb0c6973bb031a6679fccab259218256caf1cdee
Jesse Chan <jesse.chan@rice.edu>
2020-04-23 12:10:50 -0500

    update

--- dg_burgers2D_quad.jl
+++ dg_burgers2D_quad.jl
@@ -17,8 +17,8 @@ using Setup2DQuad
 "Define approximation parameters"
 N   = 4 # The order of approximation
 K1D = 8 # number of elements along each edge of a rectangle
-CFL = .25 # relative size of a time-step
-T   = .1 # final time
+CFL = .125 # relative size of a time-step
+T   = .25 # final time
 
 "=========== Setup code ============="
 
@@ -26,8 +26,10 @@ T   = .1 # final time
 VX,VY,EToV = uniform_quad_mesh(K1D,K1D)
 
 iids = findall(@. (abs(abs(VX)-1) > 1e-12) & (abs(abs(VY)-1) > 1e-12))
-@. VX[iids] += .1/K1D*randn()
-@. VY[iids] += .1/K1D*randn()
+# @. VX[iids] += .1/K1D*randn()
+# @. VY[iids] += .1/K1D*randn()
+VX = @. VX + .25*sin(pi*VX)*sin(pi*VY)
+VY = @. VY + .25*sin(pi*VX)*sin(pi*VY)
 
 # intialize reference operators
 rd = init_reference_quad(N)
@@ -50,10 +52,12 @@ mapP[mapB] = mapPB
 M = transpose(Vq)*diagm(wq)*Vq
 
 # redefine quadrature operators
-rq,sq = (r,s)
+rq,sq = (r,s) # lobatto points
 wq = vec(sum(M,dims=2)) # apply mass lumping to get integrals
 Vq = vandermonde_2D(N,rq,sq)/V
 M  = transpose(Vq)*diagm(wq)*Vq
+Pq = M\(Vq'*diagm(wq))
+# Qr, Qs do not need to be modified
 
 # redefine gauss lobatto quadrature
 r1D,w1D = gauss_lobatto_quad(0,0,N)
@@ -63,7 +67,6 @@ sf = [-e; r1D; e; -r1D]
 wf = vec(repeat(w1D,Nfaces,1));
 Vf = vandermonde_2D(N,rf,sf)/V
 LIFT = M\(Vf'*diagm(wf))
-Pq = M\(Vq'*diagm(wq))
 @pack! rd = Vf,Vq,Pq,LIFT
 
 "======== Define initial coefficients and time-stepping =========="
@@ -93,22 +96,24 @@ function rhs(u, rd::RefElemData, md::MeshData)
     # dudx   = @. rxJ*ur + sxJ*us
     # dudy   = @. ryJ*ur + syJ*us
     f_proj = (u.^2)
-    #
     # du2x  = rxJ.*(Dr*f_proj) + sxJ.*(Ds*f_proj)
     # du2y  = ryJ.*(Dr*f_proj) + syJ.*(Ds*f_proj)
+
     dudx,dudy,du2x,du2y = ntuple(x->zeros(size(u)),4)
     for e = 1:K
+
         Dx = .5*(diagm(rxJ[:,e])*Dr + diagm(sxJ[:,e])*Ds
             + Dr*diagm(rxJ[:,e]) + Ds*diagm(sxJ[:,e]))
         Dy = .5*(diagm(ryJ[:,e])*Dr + diagm(syJ[:,e])*Ds
             + Dr*diagm(ryJ[:,e]) + Ds*diagm(syJ[:,e]))
+
         dudx[:,e] = Dx*u[:,e]
         dudy[:,e] = Dy*u[:,e]
         du2x[:,e] = Dx*f_proj[:,e]
         du2y[:,e] = Dy*f_proj[:,e]
     end
     du2   = du2x + du2y # (du^2/dx + du^2/dy, v)
-    udu   = u.*(dudx+dudy) # (u*dudx,v)
+    udu   = u.*(dudx+dudy) # (u*dudx + u*dudy,v)
 
     # boundary terms
     uf = Vf*u
@@ -146,7 +151,7 @@ for i = 1:Nsteps
         rhsQ    = rhs(u,rd,md)
 
         if INTRK==5
-            rhstest[i] = sum(u.*(J.*(M*rhsQ)))
+            rhstest[i] = sum(u.*(J.*(M*rhsQ))) # (du/dt,u) + [(RHS(u), u)?=0]
         end
 
         @. resQ = rk4a[INTRK]*resQ + dt*rhsQ
@@ -164,21 +169,21 @@ display(scatter(xp,yp,vv,zcolor=vv,camera=(0,90)))
 
 @show maximum(abs.(rhstest))
 
-function burgers_exact_sol_2D(u0,x,y,T,dt)
-    Nsteps = ceil(Int,T/dt)
-    dt = T/Nsteps
-    u = u0(x,y) # computed at input points
-    for i = 1:Nsteps
-        t = i*dt
-        u .= @. u0(x-u*t,y-u*t) # evolve solution at quadrature points using characteristics
-    end
-    return u
-end
-
-@unpack J = md
-rq2,sq2,wq2 = quad_nodes_2D(3*N)
-Vq2 = vandermonde_2D(N,rq2,sq2)/V
-xq2,yq2 = (x->Vq2*x).((x,y))
-wJq2 = diagm(wq2)*(Vq2*J)
-L2err = sqrt(sum(wJq2.*(Vq2*u - burgers_exact_sol_2D(u0,xq2,yq2,T,dt/250)).^2))
-@show L2err
+# function burgers_exact_sol_2D(u0,x,y,T,dt)
+#     Nsteps = ceil(Int,T/dt)
+#     dt = T/Nsteps
+#     u = u0(x,y) # computed at input points
+#     for i = 1:Nsteps
+#         t = i*dt
+#         u .= @. u0(x-u*t,y-u*t) # evolve solution at quadrature points using characteristics
+#     end
+#     return u
+# end
+#
+# @unpack J = md
+# rq2,sq2,wq2 = quad_nodes_2D(3*N)
+# Vq2 = vandermonde_2D(N,rq2,sq2)/V
+# xq2,yq2 = (x->Vq2*x).((x,y))
+# wJq2 = diagm(wq2)*(Vq2*J)
+# L2err = sqrt(sum(wJq2.*(Vq2*u - burgers_exact_sol_2D(u0,xq2,yq2,T,dt/250)).^2))
+# @show L2err
