da5642dafc0afd9764b82456a1806f87b9b7c0e1
yiminllin <yiminlllin@gmail.com>
2020-06-12 15:31:43 -0500

    ES-Fourier test GPU.jl

---
+++ examples/ES_Fourier-DG/test_GPU.jl
@@ -0,0 +1,54 @@
+using KernelAbstractions, Test, CUDAapi
+if CUDAapi.has_cuda_gpu()
+    using CuArrays
+    CuArrays.allowscalar(false)
+end
+
+# Simple kernel for matrix multiplication
+@kernel function matmul_kernel!(a, b, c)
+    i, j = @index(Global, NTuple)
+
+    # creating a temporary sum variable for matrix multiplication
+    tmp_sum = zero(eltype(c))
+    for k = 1:size(a)[2]
+        tmp_sum += a[i,k] * b[k, j]
+    end
+
+    c[i,j] = tmp_sum
+end
+
+# Creating a wrapper kernel for launching with error checks
+function matmul!(a, b, c)
+    if size(a)[2] != size(b)[1]
+        println("Matrix size mismatch!")
+        return nothing
+    end
+    if isa(a, Array)
+        kernel! = matmul_kernel!(CPU(),4)
+    else
+        kernel! = matmul_kernel!(CUDA(),256)
+    end
+    kernel!(a, b, c, ndrange=size(c))
+end
+
+a = rand(256,123)
+b = rand(123, 45)
+c = zeros(256, 45)
+
+# beginning CPU tests, returns event
+ev = matmul!(a,b,c)
+wait(ev)
+
+@test isapprox(c, a*b)
+
+# beginning GPU tests
+if has_cuda_gpu()
+    d_a = CuArray(a)
+    d_b = CuArray(b)
+    d_c = CuArray(c)
+
+    ev = matmul!(d_a, d_b, d_c)
+    wait(ev)
+
+    @test isapprox(Array(d_c), a*b)
+end
