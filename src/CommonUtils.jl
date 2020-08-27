b48f31be82642013575565b4542b99407935164b
Jesse Chan <jesse.chan@rice.edu>
2020-08-26 21:11:57 -0500

    adding comments

--- src/CommonUtils.jl
+++ src/CommonUtils.jl
@@ -19,13 +19,17 @@ export build_node_maps
 export build_periodic_boundary_maps, build_periodic_boundary_maps!
 export rk45_coeffs, dopri45_coeffs
 
-export unzip # convert array of tuples to tuples of arrays
+# convert array of tuples to tuples of arrays
+export unzip
 unzip(a) = map(x->getfield.(a, x), fieldnames(eltype(a)))
+
+# Convenience routines for identity matrices.
+# Pair with kron to make block diagonal or banded matrices
 export eye,speye
 eye(n) = diagm(ones(n))
 speye(n) = spdiagm(0 => ones(n))
 
-# convenience routines
+# convenience routines used for broadcasting over tuples of arrays
 export bcopy!, bmult
 bcopy!(x,y) = x .= y
 bmult(x,y) = x .* y
@@ -61,13 +65,7 @@ function dopri45_coeffs()
             19372.0/6561.0 -25360.0/2187.0  64448.0/6561.0  -212.0/729.0    0.0             0.0         0.0
             9017.0/3168.0  -355.0/33.0      46732.0/5247.0  49.0/176.0      -5103.0/18656.0 0.0         0.0
             35.0/384.0      0.0             500.0/1113.0    125.0/192.0     -2187.0/6784.0  11.0/84.0   0.0 ]
-    # rk4a = [0.2             0.0             0.0             0.0             0.0             0.0
-    #         3.0/40.0        9.0/40.0        0.0             0.0             0.0             0.0
-    #         44.0/45.0      -56.0/15.0       32.0/9.0        0.0             0.0             0.0
-    #         19372.0/6561.0 -25360.0/2187.0  64448.0/6561.0  -212.0/729.0    0.0             0.0
-    #         9017.0/3168.0  -355.0/33.0      46732.0/5247.0  49.0/176.0      -5103.0/18656.0 0.0
-    #         35.0/384.0      0.0             500.0/1113.0    125.0/192.0     -2187.0/6784.0  11.0/84.0  ]
-
+    
     rk4c = vec([0.0 0.2 0.3 0.8 8.0/9.0 1.0 1.0 ])
 
     # coefficients to evolve error estimator
