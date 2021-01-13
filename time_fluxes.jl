231663fec109407e3510ea3fa1fe4a160ca7bec0
Jesse Chan <jchan985@gmail.com>
2021-01-12 23:56:20 -0600

    upd to flux timings

--- time_fluxes.jl
+++ time_fluxes.jl
@@ -47,7 +47,7 @@ function jac!(dfdu,u,dF,QTr)
     end
 end
 
-u = 1 .+ rand(K)
+u = randn(K)
 cache = FiniteDiff.JacobianCache(u)
 output = zeros(size(Q))
 f!(out,u) = ff!(out,u,F,Q)
@@ -61,43 +61,3 @@ f(u) = ff(u,Q,F)
 # @btime FiniteDiff.finite_difference_jacobian!($output,$f!,$u,$cache) # FiniteDiff.jl
 # @btime jac!($output,$u,$dF,$Q) # explicit Jacobian
 # out = similar(u); @btime f!($out,$u) # flux eval
-
-function LF(uL,uR)
-        return (@. .5*max(abs(uL),abs(uR))*(uL-uR))
-        # return (@. (uL-uR))
-end
-B = randn(K,K)
-B = B'*B
-# B = [0 1;1 0]
-d!(out,u) = ff!(out,u,LF,B)
-FiniteDiff.finite_difference_jacobian!(output,d!,u,cache)
-
-uR,uL = meshgrid(u)
-dxLF(uL,uR) = ForwardDiff.derivative(uL->LF(uL,uR),uL)
-dyLF(uL,uR) = ForwardDiff.derivative(uR->LF(uL,uR),uR)
-jacx = -B.*transpose(dxLF.(uL,uR)) + diagm(vec(sum(B.* transpose(dxLF.(uL,uR)),dims=1)))
-jacy = B.*dyLF.(uL,uR) - diagm(vec(sum(B.*dyLF.(uL,uR),dims=1)))
-
-dxLFJ(uL,uR) = ForwardDiff.jacobian(uL->LF(uL,uR),uL)
-dyLFJ(uL,uR) = ForwardDiff.jacobian(uR->LF(uL,uR),uR)
-jacx2 = Matrix(hadamard_jacobian(sparse(B), dxLFJ, [u]))
-jacy2 = Matrix(hadamard_jacobian(sparse(B), dyLFJ, [u]))
-
-@show norm(output - jacx), norm(jacx - jacx2)
-@show norm(output - jacy), norm(jacy - jacy2)
-
-# F(x,y)  = SVector{2}(x[1]*y[2],y[1]+x[2])
-# dF(x,y) = ForwardDiff.jacobian(y->F(x,y),y)
-# uL = @SVector [.1 .+ rand(10000),.1 .+ rand(10000)]
-# uR = @SVector [.1 .+ rand(10000),.1 .+ rand(10000)]
-#
-# function feval(uL,uR,F)
-#     for i = 1:length(uL)
-#         uLi = getindex.(uL,i)
-#         uRi = getindex.(uR,i)
-#         F(uLi,uRi)
-#     end
-# end
-#
-# @btime feval($uL,$uR,$F)
-# @btime feval($uL,$uR,$dF)
