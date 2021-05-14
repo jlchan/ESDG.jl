96755f71a69f5c2daa71bb59ebd38d1cc70c44ca
Jesse Chan <jesse.chan@rice.edu>
2021-05-13 20:53:44 -0500

    removing Plot dependencies, moving tmap into ESDG.jl

--- src/ESDG.jl
+++ src/ESDG.jl
@@ -1,17 +1,28 @@
 module ESDG
 
+using NodesAndModes
 using StartUpDG
+
+using StructArrays: components,foreachfield
 
 using CheapThreads
 using MAT # convert from .mat files 
 using Triangulate, Printf
 
-using Triangulate, Printf
-using Plots: plot,plot!
-import Triangulate:triangulate
+export tmap!
+@inline function tmap!(f,out,x)
+    @batch for i in eachindex(x)
+        @inbounds out[i] = f(x[i])
+    end
+end
+
+
+
+# support for triangular meshes via Triangulate.jl (Jonathan Shewchuk's Triangle software)
 export triangulateIO_to_VXYEToV,get_node_boundary_tags,
        plot_mesh,plot_mesh!,plot_segment_tags,plot_segment_tags!
 include("triangulate_utils.jl")
+
 export rectangular_domain,square_domain,square_hole_domain,scramjet,refine
 include("triangulate_example_meshes.jl")
 
