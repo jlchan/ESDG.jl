17f481cc5e94c5d9bfddab9a2615c6f7e50de7e3
Jesse Chan <jesse.chan@rice.edu>
2021-05-21 16:59:22 -0500

    yak shaving reshuffling of ode_utils.jl

--- src/ode_utils.jl
+++ src/ode_utils.jl
@@ -1,3 +1,5 @@
+default_monitor(integrator) = println("on timestep $(integrator.iter), at time $(integrator.t).")
+
 """
     function monitoring_callback(interval=10, monitor=default_monitor)
 
@@ -8,11 +10,10 @@ See https://diffeq.sciml.ai/stable/basics/integrator/#Handing-Integrators for `i
 WARNING - don't modify any `integrator` fields within `monitor` or it may ruin the ODE solve.
 """
 function monitor_callback(interval=10, monitor=default_monitor)
-    condition(u,t,integrator) = integrator.iter % interval ==0 || isfinished(integrator)
+    condition(u,t,integrator) = (interval > 0) && (integrator.iter % interval ==0 || isfinished(integrator))
     return DiscreteCallback(condition, monitor, save_positions=(false,false))
 end
 
-default_monitor(integrator) = println("on timestep $(integrator.iter), at time $(integrator.t).")
 
 # copied from https://github.com/trixi-framework/Trixi.jl/blob/99a7e2840872d0f1180a984dc034ec81be462a0b/src/callbacks_step/callbacks_step.jl#L17-L23
 @inline function isfinished(integrator)
