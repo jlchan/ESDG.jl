75d20ff981a798b1b64f3044869efb35623a7aee
Jesse Chan <jesse.chan@rice.edu>
2021-05-21 14:58:47 -0500

    trimming cruft

--- examples/test/testTriplot.jl
+++ examples/test/testTriplot.jl
@@ -1,76 +1,92 @@
+using TriplotRecipes
+import TriplotRecipes: DGTriPseudocolor
+
+"""
+function plotting_interpolation_matrix(Nplot,rd)
+
+Computes matrix which interpolates from reference interpolation points to equispaced points of degree `Nplot`.
+"""
+function plotting_interpolation_matrix(Nplot,rd)
+    rp,sp = NodesAndModes.equi_nodes(rd.elemShape,Nplot)
+    Vp = NodesAndModes.vandermonde(rd.elemShape,rd.N,rp,sp) / rd.VDM
+    return Vp
+end
+
+"""
+    function DGTriPseudocolor(u_plot,rd::RefElemData,md::MeshData)
+
+Plots `u_plot` the solution on a triangular mesh. Assumes `size(u_plot,1)==size(rd.Vp,1)`, e.g., 
+`u_plot` is evaluated at the plotting nodes already. 
+"""
+TriplotRecipes.DGTriPseudocolor(u_plot,rd::RefElemData,md::MeshData) = DGTriPseudocolor(u_plot,rd.Vp,rd,md)
+
+"""
+    function DGTriPseudocolor(u, Nplot::Int,rd::RefElemData,md::MeshData)
+
+Interpolates solution `u` to a polynomial of degree `Nplot`. Assumes `size(u_plot,1)==size(rd.Vp,2)`, e.g., 
+`u` is evaluated at the nodal points `md.x`, `md.y` and not plotting points.
+"""
+function TriplotRecipes.DGTriPseudocolor(u,Nplot::Int,rd::RefElemData,md::MeshData) 
+    Vp = plotting_interpolation_matrix(Nplot,rd)
+    return DGTriPseudocolor(Vp*u,Vp,rd,md)
+end
+
+"""
+    function TriplotRecipes.DGTriPseudocolor(u_plot,Vp::Matrix,rd::RefElemData,md::MeshData)
+
+Returns a DGTriPseudocolor plot recipe from TriplotRecipes. 
+Inputs: 
+    - u_plot = matrix of size (Nplot,K) representing solution to plot. 
+    - Vp = interpolation matrix of size (Nplot,Np). 
+"""
+function TriplotRecipes.DGTriPseudocolor(u_plot,Vp::Matrix,rd::RefElemData,md::MeshData)
+
+    @assert size(Vp,1) == size(u_plot,1) "Row dimension of u_plot does not match row dimension of Vp"
+
+    # plotting nodes implicitly defined by Vp and rd::RefElemData
+    rp,sp = Vp*rd.r, Vp*rd.s
+
+    # triangulate plotting nodes on ref element (fast because thanks J. Shewchuk!)
+    triin = Triangulate.TriangulateIO()
+    triin.pointlist = permutedims([rp sp]) 
+    triout_plot,_ = triangulate("Q", triin)
+    t = triout_plot.trianglelist # reference element plotting triangulation
+
+    # build discontinuous data on plotting triangular mesh 
+    num_ref_elements = size(t,2)
+    num_elements_total = num_ref_elements * md.K
+    tp = zeros(Int,3,num_elements_total)
+    zp = similar(tp,eltype(zz))
+    for e = 1:md.K
+        for i = 1:size(t,2)
+            tp[:,i + (e-1)*num_ref_elements] .= @views t[:,i] .+ (e-1)*size(Vp,1)
+            zp[:,i + (e-1)*num_ref_elements] .= @views u_plot[t[:,i],e]
+        end
+    end
+    return DGTriPseudocolor(vec(Vp*md.x),vec(Vp*md.y),zp,tp)
+end
+
+
+
 using Plots
 using Triangulate
 using ESDG
-using TriplotRecipes
 
 N = 3
 Nplot = N+1
-elementType = Quad()
+elementType = Tri()
 rd = RefElemData(elementType,N)
-# triout = scramjet()
-# VX,VY,EToV = triangulateIO_to_VXYEToV(triout)
-VX,VY,EToV = uniform_mesh(elementType,16)
+# VX,VY,EToV = triangulateIO_to_VXYEToV(scramjet())
+VX,VY,EToV = uniform_mesh(elementType,200)
 md = MeshData(VX,VY,EToV,rd)
-
-# submesh visualization 
-rp,sp = equi_nodes(elementType,Nplot)
-Vp = NodesAndModes.vandermonde(elementType,N,rp,sp) / NodesAndModes.vandermonde(elementType,N,rd.r,rd.s)
-triin = Triangulate.TriangulateIO()
-triin.pointlist = permutedims([rp sp]) # hcat(unique([ Cdouble[rand(1:raster)/raster, rand(1:raster)/raster] for i in 1:n])...)
-triout_plot,_ = triangulate("Q", triin)
-# t = permutedims(triout.trianglelist) # mesh list
-t = triout_plot.trianglelist # mesh list
 
 @unpack x,y = md
 u = @. 2 + sin(2.5*pi*x)*exp(x+sin(pi*y))
-zz = Vp*u
-z_extrema = extrema(zz)
 
-num_ref_elements = size(t,2)
-num_elements_total = num_ref_elements * md.K
-tp = zeros(Int,3,num_elements_total)
-zp = similar(tp,eltype(zz))
-for e = 1:md.K
-    for i = 1:size(t,2)
-        tp[:,i + (e-1)*num_ref_elements] .= @views t[:,i] .+ (e-1)*size(Vp,1)
-        zp[:,i + (e-1)*num_ref_elements] .= @views zz[t[:,i],e]
-    end
-end
+# Plots.plot(DGTriPseudocolor(rd.Vp*u,rd,md),color=:blues)
+Plots.plot(DGTriPseudocolor(u,1,rd,md),color=:blues)
 
-dgtripcolor(vec(Vp*md.x),vec(Vp*md.y),zp,tp,color=:blues)
+plot(DGTriPseudocolor(u,2,rd,md))
 
-# trimesh!(VX,VY,EToV,fillalpha=0.0,linecolor=:white,linewidth=.1,aspect_ratio=:equal)
-# # Plots.jl version - try NaN separators when Will implements them
-# Plots.plot(aspect_ratio=:equal,size=(800,720)) 
-# dgtripcolor!(xp,yp,zz,tp,color=:magma)
-# for e = 1:md.K
-#     xye = view.(md.xyz,:,e)
-#     zze = view(zz,:,e)
-#     # coordinates = [(x->Vp*x).(xye)... zze]
-#     tripcolor!((x->Vp*x).(xye)...,zze,t,color=:magma)
-#     # dgtripcolor!(x,y,zze,t,color=:magma)
-#     # trimesh!(x,y,t,fillalpha=0.0,linecolor=:white)
-# end
-# display(Plots.plot!())
-
-
-
-
-# using Plots,DelimitedFiles,TriplotRecipes
-
-# f(x,y) = exp(0.1*sin(5.1*x + -6.2*y) + 0.3*cos(4.3*x + 3.4*y))
-
-# x,y = eachcol(readdlm(joinpath(@__DIR__,"dolphin.xy")))
-# t = readdlm(joinpath(@__DIR__,"dolphin.t"),Int)' .+ 1
-# # z = f.(x,y)
-# # z_dg = similar(t, eltype(z))
-
-# for it=1:size(t,2)
-#     z_dg[:,it] .= z[t[:,it]] .+ 0.05*(rand() - 0.5)
-# end
-
-# Plots.plot(aspect_ratio=:equal,size=(800,720))
-# tripcolor!(x,y,z,t,color=:magma)
-# # dgtripcolor!(x,y,z_dg,t,color=:magma)
-# trimesh!(x,y,t,fillalpha=0.0,linecolor=:white)
-# savefig("dolphin_dg.png")
+# Vp = plotting_interpolation_matrix(1,rd)
+# plot(DGTriPseudocolor(Vp*u,Vp,rd,md))
