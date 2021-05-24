9ce85177ce5f581f5d16653a131684773550bed0
Jesse Chan <jesse.chan@rice.edu>
2021-05-23 21:30:44 -0500

    adding different construction of boundary tags

--- examples/compressible_Euler/esdgsem.jl
+++ examples/compressible_Euler/esdgsem.jl
@@ -7,7 +7,7 @@ using ESDG
 using TimerOutputs # note: this is the NullOutput branch
 
 N = 3
-K1D = 20
+K1D = 32
 CFL = .1
 rd = RefElemData(Quad(), N)
 sbp = DiagESummationByParts!(Quad(),N,rd) # modify rd 
@@ -15,19 +15,46 @@ VX,VY,EToV = uniform_mesh(Quad(),K1D)
 md = MeshData(VX,VY,EToV,rd)
 # md = make_periodic(md,rd)
 
-# make tags for boundary conditions
+# tags for boundary conditions
 is_boundary_node = zeros(Int,size(md.xf))
 is_boundary_node[md.mapB] .= 1 # change to boundary tag later
 
-function Dirichlet_boundary_state(equation::EntropyStableEuler.Euler{2},normal,UM)
-    ϱ, ϱu, ϱv, E = UM
-    u, v = ϱu/ϱ, ϱv/ϱ
-    nx, ny = normal
-    u_n = u*nx + v*ny
-    uP = u - 2*u_n*nx
-    vP = v - 2*u_n*ny
-    return SVector{4}(ϱ,ϱ*uP,ϱ*vP,E) 
+# get face centroids 
+boundary_face_ids = findall(vec(md.FToF) .== 1:length(md.FToF))
+function get_face_centroids(xf)
+    Nfp = size(md.xf,1)÷rd.Nfaces
+    xc = reshape(xf,Nfp,rd.Nfaces*md.K)
+    return typeof(xf)(sum(xc,dims=1)/size(xc,1))
 end
+xc,yc = map(get_face_centroids,md.xyzf)
+is_boundary_face = zeros(Int,rd.Nfaces*md.K)
+is_boundary_face[boundary_face_ids] .= 1
+bottom_boundary = findall(vec(@. abs(yc+1) < 50*eps()))
+# is_boundary_face[bottom_boundary] .= 2
+is_boundary_node = reshape(repeat(is_boundary_face',Nfp,1),Nfp*rd.Nfaces,md.K)
+# scatter(xc[boundary_face_ids],yc[boundary_face_ids])
+
+
+# is_boundary_node = Matrix{DataType}(undef,size(md.xf))
+# fill!(is_boundary_node,Nothing)
+# struct WallBC end
+# is_boundary_node[md.mapB] .= WallBC
+# function get_BC_state(bcType::WallBC,equation::EntropyStableEuler.Euler{2},normal,UM) 
+#     return wall_boundary_state(equation,normal,UM)
+# end
+
+
+function Dirichlet_boundary_state(equation::EntropyStableEuler.Euler{d},xyzt...) where {d}
+    return SVector{d+2}(U_function(xyzt...))
+end
+
+function pressure_outflow_state(equation::EntropyStableEuler.Euler{d},outflow_pressure,UM) where {d}
+    ρ,ρu,ρv,_ = UM
+    rho_unorm2 = (ρu^2 + ρv^2)/ρ
+    E_out = outflow_pressure / (equation.γ-1) + .5*rho_unorm2
+    return SVector{d+2}(ρ,ρu,ρv,E_out)
+end
+
 function wall_boundary_state(equation::EntropyStableEuler.Euler{2},normal,UM)
     ϱ, ϱu, ϱv, E = UM
     u, v = ϱu/ϱ, ϱv/ϱ
@@ -75,7 +102,7 @@ eqn = EntropyStableEuler.Euler{2}()
 @inline cons_to_prim_logs(u) = cons_to_prim_beta_log(EntropyStableEuler.Euler{2}(),u)
 
 timer = TimerOutput()
-# timer = NullTimer()
+timer = NullTimer()
 
 cache = (;create_cache(eqn,rd,md,sbp)..., equation=eqn, is_boundary_node, 
         fS_prim_log = two_pt_flux_logs, fS = two_pt_flux, dissipation=LxF_dissipation, 
@@ -89,22 +116,21 @@ function rhs!(dU,U,cache,t)
     @unpack cons_to_prim_logs,Uf,Qlog = cache
     @unpack timer = cache
     
-    # @timeit timer "Qlog" begin
+    @timeit timer "Primitive/log variable transforms" begin
         tmap!(cons_to_prim_logs,Qlog,U)
-        # map!(cons_to_prim_logs,Qlog,U)
-    # end
+    end
 
-    # @timeit timer "get face vals" begin
+    @timeit timer "Extract face vals" begin
         @batch for e = 1:size(U,2)
             for (i,vol_id) in enumerate(Fmask)        
                 Uf[i,e] = U[vol_id,e]        
             end
         end
-    # end
+    end
 
     @batch for e = 1:md.K
 
-        # @timeit timer "hadamard_sum" begin        
+        @timeit timer "Volume terms" begin        
             rhse = cache.rhse_threads[Threads.threadid()]
             fill!(rhse,zero(eltype(rhse)))
 
@@ -113,12 +139,20 @@ function rhs!(dU,U,cache,t)
             
             hadamard_sum_ATr!(rhse, (QxTr,QyTr), fS_prim_log, view(Qlog,:,e)) 
             # hadamard_sum_ATr!(rhse, (QxTr,QyTr), fS, view(U,:,e)) 
-        # end
+        end
 
-        # @timeit timer "interface fluxes" begin
+        @timeit timer "Interface terms" begin
             for (i,vol_id) = enumerate(Fmask)
                 UM = Uf[i,e]
                 normal = SVector{2}(nxJ[i,e], nyJ[i,e]) / sJ[i,e]
+
+                # if cache.is_boundary_node[i,e] == Nothing
+                #     UP = Uf[mapP[i,e]]
+                # else
+                #     bcType = cache.is_boundary_node[i,e]
+                #     UP = BCState(bcType,equations,getindex.(md.xyzf,i,e),t,normal,UM)
+                # end
+
                 if cache.is_boundary_node[i,e] == 1
                     UP = wall_boundary_state(equation,normal,UM)
                 else
@@ -129,12 +163,12 @@ function rhs!(dU,U,cache,t)
                 val = (Fx * nxJ[i,e] + Fy * nyJ[i,e] + diss*sJ[i,e]) * wf[i]
                 rhse[vol_id] += val
             end
-        # end
+        end
 
-        # @timeit timer "store output" begin
+        @timeit timer "Store output" begin
             @. rhse = -rhse / J[1,e] # split up broadcasts to avoid allocations
             @. dU[:,e] = invm * rhse             
-        # end
+        end
     end
 
     return nothing
@@ -146,23 +180,23 @@ U = StructArray{SVector{nvariables(eqn),Float64}}(undef,size(md.x)...)
 U .= ((x,y)->initial_condition(eqn,x,y)).(md.xyz...)
 dU = similar(U)
 
+@btime rhs!($dU,$U,$cache,$0.)
+
 # rhs!(dU,U,cache,0.)
 # reset_timer!(cache.timer)
 # for i = 1:100
 #     rhs!(dU,U,cache,0.)
 # end
+
+# h = 2/K1D
+# CN = (N+1)*(N+2)/2 
+# dt0 = CFL * h / CN
+# tspan = (0.0,.5)
+# ode = ODEProblem(rhs!,U,tspan,cache)
+# sol = solve(ode, SSPRK43(), dt=dt0, save_everystep=false, callback=monitor_callback())
+
 # show(cache.timer)
 
-h = 2/K1D
-CN = (N+1)*(N+2)/2 
-dt0 = CFL * h / CN
-tspan = (0.0,.5)
-ode = ODEProblem(rhs!,U,tspan,cache)
-sol = solve(ode, SSPRK43(), dt=dt0, save_everystep=false, callback=monitor_callback())
-
 U = sol.u[end]
-zz = vec(rd.Vp*StructArrays.component(U,1))
-scatter(vec(rd.Vp*md.x),vec(rd.Vp*md.y),zz,zcolor=zz,ms=1,msw=0,ratio=1,cam=(0,90),leg=false)
-
-# plot(DGTriPseudocolor(rd.Vp*StructArrays.component(U,1),rd,md))
+plot(DGTriPseudocolor(rd.Vp*StructArrays.component(U,1),rd,md),ratio=1,color=:blues)
 
