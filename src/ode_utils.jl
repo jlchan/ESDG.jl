5b7cc8fc6b56f8fa6840d051a0d76a3943d3df16
Jesse Chan <jesse.chan@rice.edu>
2021-05-26 01:36:33 -0500

    minor

--- src/ode_utils.jl
+++ src/ode_utils.jl
@@ -7,7 +7,7 @@ Returns a callback to monitor the progress of an ODE solver given a function `mo
 Defaults to `default_monitor`, which just prints out the timestep and current time. 
 See https://diffeq.sciml.ai/stable/basics/integrator/#Handing-Integrators for `integrator` fields. 
 
-WARNING - don't modify any `integrator` fields within `monitor` or it may ruin the ODE solve.
+WARNING - don't modify any `integrator` fields within `monitor` or it may break the ODE solve.
 """
 function monitor_callback(interval=10, monitor=default_monitor)
     condition(u,t,integrator) = (interval > 0) && (integrator.iter % interval ==0 || isfinished(integrator))
@@ -15,7 +15,8 @@ function monitor_callback(interval=10, monitor=default_monitor)
 end
 
 
-# copied from https://github.com/trixi-framework/Trixi.jl/blob/99a7e2840872d0f1180a984dc034ec81be462a0b/src/callbacks_step/callbacks_step.jl#L17-L23
+# copied directly from 
+# https://github.com/trixi-framework/Trixi.jl/blob/99a7e2840872d0f1180a984dc034ec81be462a0b/src/callbacks_step/callbacks_step.jl#L17-L23
 @inline function isfinished(integrator)
     # Checking for floating point equality is OK here as `DifferentialEquations.jl`
     # sets the time exactly to the final time in the last iteration
