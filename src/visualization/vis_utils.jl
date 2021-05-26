5850bb533c233c62ec2075eca3e886e9b273a95f
Jesse Chan <jesse.chan@rice.edu>
2021-05-25 23:17:53 -0500

    some reorg

---
+++ src/visualization/vis_utils.jl
@@ -0,0 +1,10 @@
+"""
+function plotting_interpolation_matrix(Nplot,rd)
+
+Computes matrix which interpolates from reference interpolation points to equispaced points of degree `Nplot`.
+"""
+function plotting_interpolation_matrix(Nplot,rd)
+    rp,sp = NodesAndModes.equi_nodes(rd.elemShape,Nplot)
+    Vp = NodesAndModes.vandermonde(rd.elemShape,rd.N,rp,sp) / rd.VDM
+    return Vp
+end
