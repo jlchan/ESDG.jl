5c35deb15d5928bd68cecfd8e8e55b3252c536da
Jesse Chan <jesse.chan@rice.edu>
2021-05-26 01:36:25 -0500

    updating triplot support

--- src/visualization/Triplot_support.jl
+++ src/visualization/Triplot_support.jl
@@ -8,7 +8,9 @@ include("vis_utils.jl")
 Plots `u` the solution on a 2D mesh. Assumes `size(u_plot,1)==size(rd.Vp,2)`, e.g., 
 `u` is evaluated at interpolation nodes. 
 """
-TriplotRecipes.DGTriPseudocolor(u,rd::RefElemData,md::MeshData) = TriplotRecipes.DGTriPseudocolor(rd.Vp*u,rd.Vp,rd,md)
+function TriplotRecipes.DGTriPseudocolor(u,rd::RefElemData,md::MeshData)
+    return TriplotRecipes.DGTriPseudocolor(rd.Vp*u,rd.rstp,(x->rd.Vp*x).(md.xyz))
+end
 
 """
     function DGTriPseudocolor(u, Nplot::Int,rd::RefElemData,md::MeshData)
@@ -18,7 +20,7 @@ Interpolates solution `u` to a polynomial of degree `Nplot`. Assumes `size(u_plo
 """
 function TriplotRecipes.DGTriPseudocolor(u,Nplot::Int,rd::RefElemData,md::MeshData) 
     Vp = plotting_interpolation_matrix(Nplot,rd)
-    return DGTriPseudocolor(Vp*u,Vp,rd,md)
+    return DGTriPseudocolor(Vp*u,(x->Vp*x).(rd.rst),(x->Vp*x).(md.xyz))
 end
 
 """
@@ -27,31 +29,33 @@ end
 Returns a DGTriPseudocolor plot recipe from TriplotRecipes. 
 Inputs: 
     - u_plot = matrix of size (Nplot,K) representing solution to plot. 
-    - Vp = interpolation matrix of size (Nplot,Np). 
+    - rst_plot = tuple of vector of reference plotting points of length = Nplot
+    - xyz_plot = plotting points (tuple of matrices of size (Nplot,K))
 """
-function TriplotRecipes.DGTriPseudocolor(u_plot,Vp::Matrix,rd::RefElemData,md::MeshData)
 
-    @assert size(Vp,1) == size(u_plot,1) "Row dimension of u_plot does not match row dimension of Vp"
+function TriplotRecipes.DGTriPseudocolor(u_plot, rst_plot, xyz_plot)
 
-    # plotting nodes implicitly defined by Vp and rd::RefElemData
-    rp,sp = Vp*rd.r, Vp*rd.s
+    @assert size(first(xyz_plot),1) == size(u_plot,1) "Row dimension of u_plot does not match row dimension of xyz_plot"
+    @assert size(first(rst_plot),1) == size(u_plot,1) "Row dimension of u_plot does not match row dimension of rst_plot"
+
+    Nplot,K = size(u_plot)
 
     # triangulate plotting nodes on ref element (fast because thanks J. Shewchuk!)
     triin = Triangulate.TriangulateIO()
-    triin.pointlist = permutedims([rp sp]) 
+    triin.pointlist = permutedims(hcat(rst_plot...))
     triout_plot,_ = triangulate("Q", triin)
     t = triout_plot.trianglelist # reference element plotting triangulation
 
     # build discontinuous data on plotting triangular mesh 
     num_ref_elements = size(t,2)
-    num_elements_total = num_ref_elements * md.K
+    num_elements_total = num_ref_elements * K
     tp = zeros(Int,3,num_elements_total)
     zp = similar(tp,eltype(u_plot))
-    for e = 1:md.K
+    for e = 1:K
         for i = 1:size(t,2)
-            tp[:,i + (e-1)*num_ref_elements] .= @views t[:,i] .+ (e-1)*size(Vp,1)
+            tp[:,i + (e-1)*num_ref_elements] .= @views t[:,i] .+ (e-1)*Nplot
             zp[:,i + (e-1)*num_ref_elements] .= @views u_plot[t[:,i],e]
         end
     end
-    return DGTriPseudocolor(vec(Vp*md.x),vec(Vp*md.y),zp,tp)
+    return DGTriPseudocolor(vec.(xyz_plot)...,zp,tp)
 end
