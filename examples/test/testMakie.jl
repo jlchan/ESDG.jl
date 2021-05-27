951434f2fd0d5b687d02858aa80fccaf7f8b00a5
Jesse Chan <jesse.chan@rice.edu>
2021-05-26 23:15:24 -0500

    clean up Makie test file

--- examples/test/testMakie.jl
+++ examples/test/testMakie.jl
@@ -1,62 +1,37 @@
-using GLMakie
-# using CairoMakie
+using ESDG, GLMakie, Plots
 using Triangulate
-using ESDG
-# CairoMakie.activate!()
-GLMakie.activate!()
 
 # dependencies
-using GeometryBasics, Makie 
+using GeometryBasics
 using LinearAlgebra
 
 N = 3
-Nplot = 10
-elementType = Tri()
-rd = RefElemData(elementType,N)
-md = MeshData(uniform_mesh(elementType,64)...,rd)
-
-# submesh visualization 
-rp,sp = equi_nodes(elementType,Nplot)
-Vp = NodesAndModes.vandermonde(elementType,N,rp,sp) / NodesAndModes.vandermonde(elementType,N,rd.r,rd.s)
+elementType = Quad()
+rd = RefElemData(elementType,N,Nplot=N)
+md = MeshData(uniform_mesh(elementType,100)...,rd)
 
 @unpack x,y = md
 x = @. x + .125*cos(pi/2 * (x-.5))*cos(3*pi/2*(y-.7))
 y = @. y + .125*sin(4*pi/2 * (x-.75))*cos(pi/2*(y-.8))
 
+xq,yq = rd.Vq*x,rd.Vq*y
 u = @. sin(pi*x)*sin(pi*y)*exp(-5*(x^2+y^2))
 u[:,1:md.K÷2] .+= .25
-zz = Vp*u
+# u = rd.Pq*(@. xq + yq > 0)
 
-# md_curved = @set md.xyz = (x,y);
-# function MakieMeshPlot(u,rd::RefElemData{2},md::MeshData{2})
-#     return MakieMeshPlot(rd.Vp*u,rd.Vp,rd.rst,md.xyz)
-# end
+Nfplot = Nplot+1
+rfp,sfp = StartUpDG.map_face_nodes(elementType, LinRange(-1,1,Nfplot))
+Vfp = vandermonde(elementType,N,rfp,sfp)/rd.VDM
+xf,yf = (x->Vfp*x).((x,y)) 
 
-function MakieMeshPlot(uplot,Vp,rst::NTuple{2},xyz::NTuple{2})
+# break line segments up with NaNs
+xfp,yfp,ufp = (xf->vec(vcat(reshape(xf,Nfplot,rd.Nfaces*md.K),fill(NaN,1,rd.Nfaces*md.K)))).((xf,yf,Vfp*u))
 
-    # build reference triangulation
-    rp,sp = (x->Vp*x).(rst)
-    triin = Triangulate.TriangulateIO()
-    triin.pointlist = permutedims([rp sp]) 
-    triout,_ = triangulate("Q", triin)
-    t = permutedims(triout.trianglelist) 
-    makie_triangles = Makie.to_triangles(t)
+Makie.mesh(rd.Vp*u,(x->rd.Vp*x).(rd.rst),(x->rd.Vp*x).((x,y)),color=vec(rd.Vp*u))
+# draw mesh lines above/below
+Makie.translate!(Makie.lines!(xfp,yfp,ufp,color=:white,linewidth=.5),0,0,1e-3) 
+Makie.translate!(Makie.lines!(xfp,yfp,ufp,color=:white,linewidth=.5),0,0,-1e-3)
+Makie.current_figure()
 
-    K = size(first(xyz),2)
-    trimesh = Vector{GeometryBasics.Mesh{3,Float32}}(undef,K)
-    coordinates = zeros(size(Vp,1),3)
-    for e = 1:md.K
-        xyze = view.(xyz,:,e)
-        zze = view(uplot,:,e)
-        for d = 1:length(xyze)
-            mul!(view(coordinates,:,d),Vp,xyze[d])
-        end
-        coordinates[:,3] .= zze        
-        trimesh[e] = GeometryBasics.normal_mesh(Makie.to_vertices(coordinates),makie_triangles) # speed this up?
-    end
-    return merge([trimesh...])
-end
-
-vis_mesh = MakieMeshPlot(zz,Vp,rd.rst,(x,y))
-mesh(vis_mesh,color=vec(zz))
-
+# Plots.plot(DGTriPseudocolor(rd.Vp*u,(x->rd.Vp*x).(rd.rst),(x->rd.Vp*x).((x,y))))
+# Plots.plot!(xfp,yfp,color=:white,linewidth=1,leg=false)
