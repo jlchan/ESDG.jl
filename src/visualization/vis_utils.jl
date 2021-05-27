ebe8312429ce4554bf9a616d72a972caf00037f2
Jesse Chan <jesse.chan@rice.edu>
2021-05-26 23:16:10 -0500

    remove sliver triangles for plotting

--- src/visualization/vis_utils.jl
+++ src/visualization/vis_utils.jl
@@ -9,27 +9,42 @@ function plotting_interpolation_matrix(Nplot,rd)
     return Vp
 end
 
-function triangle_area(tri)
+"""
+function compute_triangle_area(tri)
+
+Computes the area of a triangle given a tuple of three points `tri`. 
+Formula from https://en.wikipedia.org/wiki/Shoelace_formula
+"""
+function compute_triangle_area(tri)
     A,B,C = tri
     return .5*(A[1]*(B[2] - C[2]) + B[1]*(C[2]-A[2]) + C[1]*(A[2]-B[2]))
 end
 
+"""
 function plotting_triangulation(rst_plot)
-    # triangulate plotting nodes on ref element (fast because thanks J. Shewchuk!)
+
+Computes a triangulation of the points in `rst_plot`, which is a tuple containing 
+vectors of plotting points on the reference element. Returns a `3 x N_tri` matrix containing
+a triangulation of the plotting points, with zero-volume triangles removed.
+"""
+function plotting_triangulation(rst_plot,tol=50*eps())
+
+    # on-the-fly triangulation of plotting nodes on the reference element
     triin = Triangulate.TriangulateIO()
     triin.pointlist = permutedims(hcat(rst_plot...))
     triout,_ = triangulate("Q", triin)
-    t = triout.trianglelist # reference element plotting triangulation
+    t = triout.trianglelist
 
-    # filter out slivers
-    has_volume = fill(true,size(t,2))        
-    # volume = zeros(size(t,1))
-    for i = 1:size(t,2)
+    # filter out sliver triangles
+    has_volume = fill(true,size(t,2))
+    for i in axes(t,2)
         ids = @view t[:,i]
-        area = ESDG.triangle_area(zip(triout.pointlist[1,ids],triout.pointlist[2,ids]))
-        if abs(area) < 50*eps()
+        x_points = @view triout.pointlist[1,ids]
+        y_points = @view triout.pointlist[2,ids]
+        area = compute_triangle_area(zip(x_points,y_points))
+        if abs(area) < tol
             has_volume[i] = false
         end
-    end 
+    end
     return t[:,findall(has_volume)]
 end
