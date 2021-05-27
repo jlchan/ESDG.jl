223e56c5e6d74ce58bf6ba871d909e9c40dcbd7f
Jesse Chan <jesse.chan@rice.edu>
2021-05-26 23:15:48 -0500

    clean up cruft

--- src/visualization/Makie_support.jl
+++ src/visualization/Makie_support.jl
@@ -4,30 +4,9 @@ include("vis_utils.jl")
 
 # call example: mesh(Vp*u,(x->Vp*x).(rd.rst),(x->Vp*x).(md.xyz))
 function Makie.convert_arguments(P::Type{<:Makie.Mesh},uplot,rst_plot,xyz_plot)
-    # # build reference triangulation
-    # triin = Triangulate.TriangulateIO()
-    # triin.pointlist = permutedims([rp sp]) 
-    # triout,_ = triangulate("cQ", triin)
-    # t = permutedims(triout.trianglelist)
 
-    # # remove zero-volume triangles
-    # has_volume = fill(true,size(t,1))
-    # volume = zeros(size(t,1))
-    # for i = 1:size(t,1)
-    #     # randomly assign different z-values to triangle for volume calc
-    #     v1 = GeometryBasics.Point(rp[t[i,1]],sp[t[i,1]],1.0) 
-    #     v2 = GeometryBasics.Point(rp[t[i,2]],sp[t[i,2]],2.0)
-    #     v3 = GeometryBasics.Point(rp[t[i,3]],sp[t[i,3]],3.0)
-    #     volume[i] = abs(GeometryBasics.volume(Triangle(v1,v2,v3)))
-    #     if abs(GeometryBasics.volume(Triangle(v1,v2,v3))) < 50*eps()
-    #         has_volume[i] = false
-    #     end
-    # end
-    # t = t[findall(has_volume),:]
     t = permutedims(plotting_triangulation(rst_plot))
     makie_triangles = Makie.to_triangles(t)
-
-    # ref_elem_mesh = GeometryBasics.normal_mesh(Makie.to_vertices([rp sp zero.(rp)]),makie_triangles) 
 
     num_elements = size(first(xyz_plot),2)
     trimesh = Vector{GeometryBasics.Mesh{3,Float32}}(undef,num_elements)
@@ -48,11 +27,13 @@ function Makie.convert_arguments(P::Type{<:Makie.Mesh},u,Vp::Matrix,rst,xyz)
     convert_arguments(P,Vp*u,(x->Vp*x).(rst),(x->Vp*x).(xyz))
 end
 
+# specify Nplot 
 function Makie.convert_arguments(P::Type{<:Makie.Mesh},u,Nplot::Int,rd::RefElemData,md::MeshData)
     Vp = plotting_interpolation_matrix(Nplot,rd)
     return convert_arguments(P,u,Vp,rd.rst,md.xyz)
 end
 
+# specify nothing, use rd.Vp for plot interp matrix
 function Makie.convert_arguments(P::Type{<:Makie.Mesh},u,rd::RefElemData,md::MeshData)
     return convert_arguments(P,u,rd.Vp,rd.rst,md.xyz)
 end
