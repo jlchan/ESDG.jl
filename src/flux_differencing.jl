1d497eaa6bd867d119126cb8db34c811cc11be70
Jesse Chan <jesse.chan@rice.edu>
2021-06-08 19:10:27 -0500

    adding sparse matrix support to hadamard_sum_ATr!

--- src/flux_differencing.jl
+++ src/flux_differencing.jl
@@ -1,23 +1,4 @@
 """
-    function hadamard_sum_ATr!(rhs, ATr, F, u, skip_index=(i,j)->false)
-
-Computes and accumulates `sum(A.*F(u,u'),dims=2)` into `rhs`. 
-""" 
-@inline function hadamard_sum_ATr!(rhs, ATr, F, u, skip_index=(i,j)->false)
-    rows,cols = axes(ATr)
-    for i in cols
-        ui = u[i]
-        val_i = rhs[i]
-        for j in rows
-            if !skip_index(i,j)
-                val_i += ATr[j,i] * F(ui,u[j]) # breaks for tuples, OK for StaticArrays
-            end
-        end
-        rhs[i] = val_i # why not .= here?
-    end
-end
-
-"""
     function hadamard_sum_ATr!(rhs, ATr_list::NTuple{N}, F, u, skip_index=(i,j)->false) where {N}
 
 Given a tuple of operators `ATr_list` and a flux `F(UL,UR)` which returns a tuple of outputs, this will 
@@ -37,44 +18,48 @@ accumulate ∑_i `sum(A_i.*F(u,u')[i],dims=2)` into `rhs`.
     end
 end
 
-# accumulate A.*F into rhs but take advantage of sparsity of A
-@inline function hadamard_sum_ATr!(rhs, ATr::AbstractSparseMatrix, F, u)
+"""
+    function hadamard_sum_ATr!(rhs, ATr, F, u, skip_index=(i,j)->false)
+
+Computes and accumulates `sum(A.*F(u,u'),dims=2)` into `rhs`. 
+""" 
+@inline function hadamard_sum_ATr!(rhs, ATr, F, u, skip_index=(i,j)->false)
+    hadamard_sum_ATr!(rhs, (ATr,), F ∘ tuple, u, skip_index)
+end
+
+"""
+    function hadamard_sum_ATr!(rhs, ATr_list::NTuple{Dim,<:AbstractSparseMatrix}, F, u)    
+    function hadamard_sum_ATr!(rhs, ATr_list::NTuple{Dim,<:AbstractSparseMatrix}, ScalingMatrices, F, u)
+
+Accumulates sum(A.*F(u,u'),dims=2) into rhs but takes advantage of sparsity of A. 
+
+In practice, we often wish to compute `sum(A_i .* G_i .* F(u,u'),dims=2)` where `G_i` are scaling terms.
+The argument `ScalingMatrices` is a tuple of matrices containing the matrices `G_i` which scale `A_i` entrywise. 
+Suggested use: define ScalingMatrices as a tuple of LazyArrays for efficiency. 
+"""
+@inline function hadamard_sum_ATr!(rhs, ATr_list::NTuple{Dim,<:AbstractSparseMatrix}, ScalingMatrices, F, u) where {Dim}
+    for d = 1:Dim
+        F_d = let d=d
+            @inline (x,y)->F(x,y)[d]
+        end
+        _hadamard_sum_ATr!(rhs,ATr_list[d],ScalingMatrices[d],F_d,u)
+    end
+end
+
+@inline function _hadamard_sum_ATr!(rhs, ATr::AbstractSparseMatrix, ScalingMatrix, F, u)
+    n = size(ATr,2)
     rows = rowvals(ATr)
-    # vals = nonzeros(ATr)
-    for i = 1:size(ATr,2) # all ops should be same length
+    vals = nonzeros(ATr)
+    for i = 1:n 
         ui = u[i]
-        val_i = rhs[i] # accumulate into existing rhs
-        for row_id in nzrange(ATr,i)
-            j = rows[row_id]
-            val_i += A[i,j]*F(ui,u[j]) # can replace A[i,j] with vals[row_id] but not for lazy sparse combo
+        val_i = rhs[i] 
+        for id in nzrange(ATr,i)
+            j   = rows[id]
+            Aij = vals[id]
+            val_i += Aij * ScalingMatrix[j,i] * F(ui,u[j])
         end
         rhs[i] = val_i
     end
 end
-
-
-# struct SameSparsityMatrices{N,Tv,Ti}
-#     matrices::NTuple{N,SparseMatrixCSC{Tv,Ti}}
-# end
-
-# function SameSparsityMatrices(matrices...)
-#     ijvals = findnz.(matrices)
-#     getindex.(ijvals,1)
-# end
-
-# # computes A[i,j] = ∑_k f(i,j)[k] * matrices[k][i,j]
-# struct LazySparseLinearCombination{N,Tv,Ti,F <: Function} <: AbstractSparseMatrix{Tv,Ti}
-#     matrices::NTuple{N,SparseMatrixCSC{Tv,Ti}}
-#     f::F # function of (i,j), returns tuple of scalings
-# end
-# function LazySparseLinearCombination(matrices,scalings)
-#     # todo: standardize and make sure all matrices have the same
-#     return LazySparseLinearCombination(matrices,(i,j)->scalings)
-# end
-# # set_f(A::LazySparseLinearCombination)
-# Base.size(A::LazySparseLinearCombination) = size(first(A.matrices))
-# Base.getindex(A::LazySparseLinearCombination,i,j)  = sum(getindex.(A.matrices,i,j) .* A.f(i,j))
-# SparseArrays.rowvals(A::LazySparseLinearCombination) = rowvals(first(A.matrices))
-
 
 
