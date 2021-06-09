b80baa07d94ccc429fa6ed7b641583617706d9d0
Jesse Chan <jesse.chan@rice.edu>
2021-06-08 19:10:02 -0500

    CheapThreads -> Polyester renaming

--- src/misc_utils.jl
+++ src/misc_utils.jl
@@ -1,3 +1,9 @@
+
+"""
+    function tmap!(f,out,x)
+
+Threaded `map!` function using Polyester.@batch.
+"""
 @inline function tmap!(f,out,x)
     @batch for i in eachindex(x)
         @inbounds out[i] = f(x[i])
@@ -10,9 +16,7 @@ end
 
 If CheapThreads freezes, running this might fix it. Must run manually (not sure how to automatically detect freezes). 
 """
-#function resetCheapThreads()
 function resetThreads()
-    CheapThreads.reset_workers!()
-    # Polyester.reset_workers!()
+    Polyester.reset_workers!()
     ThreadingUtilities.reinitialize_tasks!()
 end
