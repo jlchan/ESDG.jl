9cc4647a5770f99675ba39fa40545050c99c48b9
Jesse Chan <jesse.chan@rice.edu>
2021-06-08 19:10:38 -0500

    removing cruft

--- src/ESDG.jl
+++ src/ESDG.jl
@@ -2,28 +2,24 @@ module ESDG
 
 using Reexport
 @reexport using StartUpDG
-# @reexport using StaticArrays
+@reexport using StaticArrays: SVector
 # @reexport using StructArrays # don't do this - doesn't export master branch?
 
+using Setfield
 using LinearAlgebra, SparseArrays 
 using MAT # for SBP node reading
 using Triangulate, Printf
 using DiffEqBase # for callbacks
 using Polyester, ThreadingUtilities
-using RecipesBase, Colors, TriplotRecipes
-using GeometryBasics, Makie 
+using RecipesBase, Colors 
+import TriplotRecipes
+# using GeometryBasics, Makie
 
 include("ode_utils.jl")
 export monitor_callback
 
 include("ModalESDG.jl")
-export hybridized_SBP_operators, ModalESDG
-
-include("DiagESBP.jl")
-export DiagESummationByParts, DiagESummationByParts!
-
-include("NodalESDG.jl")
-export NodalESDG
+export ModalESDG
 
 include("flux_differencing.jl")
 export hadamard_sum_ATr!
@@ -36,12 +32,10 @@ export triangulateIO_to_VXYEToV, get_node_boundary_tags, refine
 include("mesh/triangulate_example_meshes.jl")
 export rectangular_domain, square_domain, square_hole_domain, scramjet 
 
-include("mesh/triangulate_plotting.jl")
-export MeshPlotter, BoundaryTagPlotter # mesh plot recipes
-
+include("visualization/mesh_plotting.jl")
 include("visualization/Triplot_support.jl")
-include("visualization/Makie_support.jl")
-export plotting_interpolation_matrix, DGTriPseudocolor
+# include("visualization/Makie_support.jl")
+export plotting_interpolation_matrix, DGTriPseudocolor, MeshPlotter, BoundaryTagPlotter 
 
 include("misc_utils.jl")
 export tmap!, resetThreads
